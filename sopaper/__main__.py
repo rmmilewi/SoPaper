31bbac01ba06984e8cf9f8b1fc7ff14f48428bb5
Reed Milewicz <rmilewi@sandia.gov>
2022-05-27 10:36:13 -0600

    Added a simple API interface so we can call sopaper from within another Python script directly

--- sopaper/__main__.py
+++ sopaper/__main__.py
@@ -44,6 +44,97 @@ def get_args():
     ret = parser.parse_args()
     ret.title = ' '.join(ret.title)
     return ret
+    
+def callAPIInterface(title,url=False,directory='.',output=None):
+    """
+    Reed Note: Added a simple API interface so we can import sopaper and call it from within another Python script,
+    rather than using the command-line interface (which this API interface duplicates).
+    """
+    query = title
+    searchers = searcher.register_searcher.get_searcher_list()
+    parsers = fetcher.register_parser.get_parser_list()
+    download_candidates = []
+    if re.match('^http[s]?://', query):
+        # skip search
+        ctx = JobContext("")
+        sr = SearchResult(None, query)
+        for parser in parsers:
+            if parser.can_handle(sr):
+                parser.fetch_info(ctx, sr)      # will update title
+                download_candidates.append((parser, sr))
+    else:
+        #query = "Distinctive image features from scale-invariant keypoint"
+        ctx = JobContext(query)
+
+        search_args = list(zip(searchers, [ctx] * len(searchers)))
+        pool = Pool()
+        as_results = [pool.apply_async(searcher_run, arg) for arg in search_args]
+        #results = [searcher_run(*arg) for arg in search_args]  # for debug
+
+        url_seen = set()    # avoid repeated url
+        for s in as_results:
+            s = s.get(ukconfig.PYTHON_POOL_TIMEOUT)
+            if s is None:
+                continue
+            ctx.update_meta_dict(s['ctx_update'])
+            ctx.try_update_title_from_search_result(s)
+
+            for sr in s['results']:
+                if sr.url in url_seen:
+                    continue
+                url_seen.add(sr.url)
+                for parser in parsers:
+                    if parser.can_handle(sr):
+                        parser.fetch_info(ctx, sr)      # will update title
+                        download_candidates.append((parser, sr))
+        pool.terminate()
+
+    download_candidates = sorted(
+        download_candidates,
+        key=lambda x: x[0].priority,
+        reverse=True)
+
+    if ctx.title:
+        ctx.title = finalize_filename(ctx.title)
+    else:
+        log_info("Failed to guess paper title!")
+        ctx.title = "Unnamed Paper"
+    if url:
+        # url mode
+        print(("Results for {}:".format(ctx.title)))
+        for (_, sr) in download_candidates:
+            print((sr.url))
+        return
+
+    for (parser, sr) in download_candidates:
+        data = parser.download(sr)
+        if not data:
+            continue
+        data = pdf_compress(data)
+
+        filename = os.path.join(directory, ctx.title + ".pdf")
+        if os.path.exists(filename):
+            log_err("File \"{}\" exists! overwrite? (y/n)".format(os.path.basename(filename)))
+            resp = eval(input())
+            if resp not in ['y', 'Y']:
+                log_info("No file written. Exiting...")
+                break
+        with open(filename, 'wb') as f:
+            f.write(data)
+        if output:
+            os.rename(filename, output)
+        break
+    else:
+        log_err("Failed to download {0}".format(ctx.title))
+        return
+    if ctx.meta.get('bibtex'):
+        log_info("Bibtex:\n{}".format(ctx.meta['bibtex']))
+    if ctx.meta.get('author'):
+        log_info("Author: {0}".format(ctx.meta['author']))
+    if ctx.meta.get('citecnt'):
+        log_info("Cite count: {0}".format(ctx.meta['citecnt']))
+    log_info("Successfully downloaded to {0}".format(filename))    
+    
 
 def main():
     global args
